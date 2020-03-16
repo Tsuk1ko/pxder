4a7d31866e1b11ece397d4eb868e3cd85c4f9bf0
神代綺凜 <i@loli.best>
2020-03-17 00:22:55 +0800

    [enhance] update checker

---
+++ src/updateChecker.js
@@ -0,0 +1,45 @@
+const getLatestVersion = require('latest-version');
+const compareVersions = require('compare-versions');
+const Fse = require('fs-extra');
+const Path = require('path');
+const Tools = require('./tools');
+
+const configFileDir = require('appdata-path').getAppDataPath('pxder');
+const checkLogFile = Path.join(configFileDir, 'update.json');
+const { name, version } = require('../package.json');
+
+class UpdateChecker {
+	constructor() {
+		this.info = Tools.readJsonSafely(checkLogFile, {
+			lastCheck: 0,
+			latestVersion: '0',
+		});
+	}
+
+	async check() {
+		const agent = global.proxyAgent;
+		const latestVersion = await getLatestVersion(name, agent ? { agent } : {});
+		this.info.latestVersion = latestVersion;
+		Fse.writeJsonSync(checkLogFile, this.info);
+	}
+
+	haveUpdate() {
+		return compareVersions(this.info.latestVersion, version) > 0;
+	}
+
+	recentlyChecked() {
+		return this.info.lastCheck + 3 * 24 * 60 * 60 * 1000 < Date.now();
+	}
+
+	/**
+	 * 取得最新版本号
+	 *
+	 * @returns {string} 最新版本号
+	 * @memberof UpdateChecker
+	 */
+	getLatestVersion() {
+		return this.info.latestVersion;
+	}
+}
+
+module.exports = UpdateChecker;
